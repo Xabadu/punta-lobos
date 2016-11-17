6d712165ebc8e9058eb016b72e8f31f7317c9854
Fernando Larrañaga <xabadu@gmail.com>
2016-11-17 01:19:55 -0300

    Add arrow for non home pages

--- header.php
+++ header.php
@@ -56,13 +56,28 @@
 	<section class="container">
 		<?php do_action( 'foundationpress_after_header' );
 			putRevSlider("home"); ?>
-			<div id="slider-info">
-				<div id="slider-icons" class="float-left">
-					<a href="http://instagram.com/<?php echo get_option("los_lobos_instagram_user"); ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-					<a href="http://facebook.com/<?php echo get_option("los_lobos_facebook_user"); ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<?php if(!is_front_page()) : ?>
+				<div id="arrow-down" class="text-center">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/flecha-down.gif">
 				</div>
-				<div id="slider-author" class="float-right">
-					<p id="slider-author-name"></p>
+				<div id="slider-info">
+					<div id="slider-icons" class="float-left">
+						<a href="http://instagram.com/<?php echo get_option("los_lobos_instagram_user"); ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="http://facebook.com/<?php echo get_option("los_lobos_facebook_user"); ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					</div>
+					<div id="slider-author" class="float-right">
+						<p id="slider-author-name"></p>
+					</div>
 				</div>
-			</div>
+			<?php else : ?>
+				<div id="slider-info-home">
+					<div id="slider-icons" class="float-left">
+						<a href="http://instagram.com/<?php echo get_option("los_lobos_instagram_user"); ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="http://facebook.com/<?php echo get_option("los_lobos_facebook_user"); ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					</div>
+					<div id="slider-author" class="float-right">
+						<p id="slider-author-name"></p>
+					</div>
+				</div>
+			<?php endif; ?>
 		<?php
